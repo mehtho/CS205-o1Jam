2f4b175be718cd280a102e4ef91b7456612de87d
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2023-03-16 23:01:45 +0800

    Fixed buggy touch events

--- app/src/main/java/cs205/a3/GameActivity.java
+++ app/src/main/java/cs205/a3/GameActivity.java
@@ -2,6 +2,7 @@ package cs205.a3;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -17,39 +18,37 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = GameFullscreenBinding.inflate(getLayoutInflater());
-
         Game game = Game.game;
 
         binding.button1.setOnTouchListener((v, event) -> {
-            if(event.getAction() == MotionEvent.ACTION_UP){
-
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
                 game.tapLane(0);
                 return true;
             }
+
             return false;
         });
 
         binding.button2.setOnTouchListener((v, event) -> {
-            if(event.getAction() == MotionEvent.ACTION_UP){
-
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
                 game.tapLane(1);
                 return true;
             }
+
             return false;
         });
 
         binding.button3.setOnTouchListener((v, event) -> {
-            if(event.getAction() == MotionEvent.ACTION_UP){
-
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
                 game.tapLane(2);
                 return true;
             }
+
             return false;
         });
 
         binding.button4.setOnTouchListener((v, event) -> {
-            if(event.getAction() == MotionEvent.ACTION_UP){
-
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
                 game.tapLane(3);
                 return true;
             }
